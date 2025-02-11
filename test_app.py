91207939c3a00ee029675b6631a90e07324b2818
Nandana <nandanaedu5@gmail.com>
2025-02-11 14:34:28 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -7,7 +7,7 @@ class TestApp(unittest.TestCase):
 
     def test_hello(self):
         response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status, 200)
         self.assertEqual(response.data.decode(), "Hello, World!")
 
 if __name__ == "__main__":
